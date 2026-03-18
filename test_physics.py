99873454056d0a8a332ad4b96bed7e21264a5380
Marco Perrotta <marcoperrotta@Marcos-MacBook-Air.local>
2026-03-18 07:48:34 +0100

    Day 2: Added torque/energy monitoring

--- test_physics.py
+++ test_physics.py
@@ -1,6 +1,31 @@
-mass = 2.0
-velocity = 3.0
+import pybullet as p
+import pybullet_data
+import time
 
-kinetic_energy = 0.5 * mass * velocity**2
+p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0, 0, -9.81)
+p.loadURDF("plane.urdf")
+robot_id = p.loadURDF("kuka_iiwa/model.urdf", [0, 0, 0], useFixedBase=True)
 
-print("Kinetic Energy:", kinetic_energy)
+num_joints = p.getNumJoints(robot_id)
+slider_ids = [p.addUserDebugParameter(f"Joint_{i}", -3, 3, 0) for i in range(num_joints)]
+
+try:
+    while True:
+        p.stepSimulation()
+        total_torque = 0
+        for i in range(num_joints):
+            target_pos = p.readUserDebugParameter(slider_ids[i])
+            p.setJointMotorControl2(robot_id, i, p.POSITION_CONTROL, target_pos)
+
+            # NEW: Get the force/torque being used by this joint
+            joint_state = p.getJointState(robot_id, i)
+            total_torque += abs(joint_state[3]) # Index 3 is the applied torque
+
+        if int(time.time() * 10) % 5 == 0: # Print every half second
+            print(f"Total Effort (Torque): {total_torque:.2f} Nm")
+
+        time.sleep(1./240.)
+except KeyboardInterrupt:
+    p.disconnect()
